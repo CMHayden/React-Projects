6e196c46632bbf380957bcc3b6c4568151ce5030
cmh1 <haydencallum4@gmail.com>
2019-04-01 23:11:25 +0100

    Finishing todo app
    FInishing the todo app

--- basicToDoList/to-do-list/src/App.js
+++ basicToDoList/to-do-list/src/App.js
@@ -1,15 +1,38 @@
 import React from "react";
 import "../src/style.css";
 import TodoItem from "./components/TodoItem";
-function App() {
-  return (
-    <div className="todo-list">
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-    </div>
-  );
+import todosData from "./todosData";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: todosData
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(id) {
+    this.setState(prevState => {
+      const updatedTodos = prevState.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      });
+      return {
+        todos: updatedTodos
+      };
+    });
+  }
+
+  render() {
+    const todoItems = this.state.todos.map(item => (
+      <TodoItem key={item.id} item={item} handleChange={this.handleChange} />
+    ));
+
+    return <div className="todo-list">{todoItems}</div>;
+  }
 }
 
 export default App;
---
+++ basicToDoList/to-do-list/src/todosData.js
@@ -0,0 +1,29 @@
+const todosData = [
+  {
+    id: 1,
+    text: "Learn React",
+    completed: false
+  },
+  {
+    id: 2,
+    text: "Study for exams",
+    completed: false
+  },
+  {
+    id: 3,
+    text: "Clean shave",
+    completed: true
+  },
+  {
+    id: 4,
+    text: "Go gym",
+    completed: true
+  },
+  {
+    id: 5,
+    text: "Learn raspberry pi",
+    completed: false
+  }
+];
+
+export default todosData;

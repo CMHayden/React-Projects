cc3854a8e8e7efc42cdba718dbf33ce9c891bbaa
cmh1 <haydencallum4@gmail.com>
2019-04-02 20:23:41 +0100

    Creating a meme generator

---
+++ memeGenerator/meme-generator/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <img
+        src="http://www.stickpng.com/assets/images/580b585b2edbce24c47b2a2c.png"
+        alt="Problem?"
+      />
+      <p>Meme Generator</p>
+    </header>
+  );
+}
+
+export default Header;
